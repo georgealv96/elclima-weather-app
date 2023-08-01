66d2b6f4d124424ace028eaadedeb3a8e5c7c490
georgealv96 <george1996alvarado@gmail.com>
2023-08-01 08:17:08 -0600

    setting up the login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,30 @@
 import { Route, Routes, Navigate } from 'react-router-dom'
 import './App.css'
+import { useState } from 'react'
 
 import HomePage from './pages/HomePage/HomePage'
 import LoginPage from './pages/LoginPage/LoginPage'
 import SignupPage from './pages/SignupPage/SignupPage'
 import LocationPage from './pages/LocationPage/LocationPage'
 import PinnedLocationsPage from './pages/PinnedLocationsPage/PinnedLocationsPage'
+import userService from './utils/userService'
 
 function App() {
+  // Defining the state that will hold the logged in user if it finds a token or null if it does not
+  const [user, setUser] = useState(userService.getUser())
+
+  // This function will update the user state every time someone logs in or signs up (it's called in LoginPage and SignupPage when the submit button is being handled)
+  function handleSignUpOrLogin() {
+    setUser(userService.getUser())
+  }
+
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path="/login" element={<LoginPage />} />
+      <Route
+        path="/login"
+        element={<LoginPage handleSignUpOrLogin={handleSignUpOrLogin} />}
+      />
       <Route path="/signup" element={<SignupPage />} />
       <Route path="/mylocations" element={<PinnedLocationsPage />} />
       <Route path="/:id" element={<LocationPage />} />
--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,12 +1,78 @@
-import React from 'react';
-import './LoginPage.css';
+import React from 'react'
+import './LoginPage.css'
+import {
+  Button,
+  Form,
+  Grid,
+  Header,
+  Image,
+  Message,
+  Segment
+} from 'semantic-ui-react'
+import { useState } from 'react'
+import userService from '../../utils/userService'
 
+export default function LoginPage({ handleSignUpOrLogin }) {
+  // Defining the state that updates changes in the form
+  const [state, setState] = useState({
+    email: '',
+    password: ''
+  })
 
-export default function LoginPage(props){
-   
+  const [error, setError] = useState('')
 
-    return (
-      <div>Login Pageeeeee</div>
-      );
+  // Defining the function that handles any change in the form
+  function handleChange(e) {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault()
+
+    try {
+      // Making the HTTP request to the server
+      await userService.login(state)
+      handleSignUpOrLogin()
+    } catch (err) {
+      setError('Check terminal and console')
+      console.log(err)
+    }
+  }
+
+  return (
+    <Grid textAlign="center" style={{ height: '100vh' }} verticalAlign="middle">
+      <Grid.Column style={{ maxWidth: 450 }}>
+        <Header as="h2" color="red" textAlign="center">
+          <Image src="/logo.png" /> Log in to your account
+        </Header>
+        <Form size="large" onSubmit={handleSubmit}>
+          <Segment stacked>
+            <Form.Input
+              type="email"
+              name="email"
+              value={state.email}
+              placeholder="E-mail address"
+              onChange={handleChange}
+              required
+            />
+            <Form.Input
+              type="password"
+              name="password"
+              value={state.password}
+              placeholder="password"
+              onChange={handleChange}
+              required
+            />
+            <Button color="yellow" fluid size="large">
+              Login
+            </Button>
+          </Segment>
+        </Form>
+        <Message>New to us? Sign Up</Message>
+      </Grid.Column>
+    </Grid>
+  )
 }
-
